4edeec43562037e55036019b1ce730c94dd9efd9
Jonathan LA <jonla8@yahoo.ca>
2015-06-29 10:53:25 -0400

    Modified naming conventions

--- Scripts/spikeWallTrigger.cs
+++ Scripts/spikeWallTrigger.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class spikeWallTrigger : MonoBehaviour {
+public class SpikeWallTrigger : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
